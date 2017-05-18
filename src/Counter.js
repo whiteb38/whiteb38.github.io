af8942027b395fd8cb4da7f067f51852ca304b9c
Brendon White <Whiteb38@gmail.com>
2017-05-17 23:50:49 -0400

    adding example

--- src/Counter.js
+++ src/Counter.js
@@ -7,17 +7,42 @@ class Counter extends React.Component {
     super();
     this.state= {
       count: 0,
+      amount: 0,
     };
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleSubtract = this.handleSubtract.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleAdd(e){
+    let countAdd = this.state.count;
+    let amountAdd = this.state.amount;
+    let sum = countAdd + amountAdd;
+    this.setState({count: sum});
+  }
+  handleSubtract(e){
+    this.setState({count: this.state.count - this.state.amount});
+  }
+  handleChange(e){
+    this.setState({amount: e.target.value});
   }
   render(){
     return(
-      <button
-        onClick={() => {
-          this.setState({ count: this.state.count +1});
-        }}
-      >
-        Count: {this.state.count}
-      </button>
+      <div>
+        <a>{this.state.count}</a>
+        <form>
+          <label>Amount:
+            <input type="text" value={this.state.amount} onChange={this.handleChange}/>
+          </label>
+        </form>
+        <div>
+          <button onClick={this.handleAdd}>
+            +
+          </button>
+          <button onClick={this.handleSubtract}>
+            -
+          </button>
+        </div>
+      </div>
     );
   }
 }
