d22d0e87274541e09ec66481c82f4a7acd40235a
Brendon White <Whiteb38@gmail.com>
2017-05-22 01:38:36 -0400

    responsive navbar

--- src/Header.js
+++ src/Header.js
@@ -10,8 +10,12 @@ class Header extends React.Component {
   render(){
       var headerStyle = {color: "#333", fontVariant: "small-caps"}
     return(
-
-          <nav className="navbar navbar-default" style={{height: "60px", borderRadius: "50px", borderWidth: "2px", margin:"0px auto"}}>
+        <div>
+          <div className="visible-xs">
+            <img className="visble-xs" data-src="holder.js/auto" src='../extra/bw_logo.png' style={{width:"110px", height:"83px",  backgroundRepeat:"no-repeat"}}>
+            </img>
+          </div>
+          <nav className="navbar navbar-default hidden-xs" style={{height: "60px", borderRadius: "50px", borderWidth: "2px", margin:"0px auto"}}>
             <div className="nav navbar-nav navbar-left">
               <ul className="nav navbar-nav navbar-left" style={{paddingTop: "2px"}}>
                 <li>
@@ -22,7 +26,7 @@ class Header extends React.Component {
           </div>
             <div className="container" style={{marginLeft:"100px"}}>
               <div className="navbar-header" style={{paddingTop: "5px"}}>
-                <a className="navbar-brand" href="#" style={headerStyle}>Brendon White</a>
+                <a className="navbar-brand hidden-xs" href="#" style={headerStyle}>Brendon White</a>
               </div>
               <div className="navbar-collapse collapse" id="navbar">
                 <ul className="nav navbar-nav" style={{paddingTop: "5px" }}>
@@ -33,19 +37,20 @@ class Header extends React.Component {
                     <a href="#ProjectTwo" style={headerStyle}>Project 2</a>
                   </li>
                 </ul>
-                <ul className="nav navbar-nav navbar-right" style={{paddingTop: "5px"}}>
+                <ul className="nav navbar-nav navbar-right hidden-xs hidden-sm hidden-md" style={{paddingTop: "5px", marginRight:"80px"}}>
                   <li>
-                    <a className="navbar-text navbar-right" href="https://www.github.com/whiteb38" target="_blank" style={{backgroundImage:"url(../extra/Webp.net-resizeimage.png)", width:"90px", height:"20px", backgroundRepeat: "no-repeat", marginTop:"6px", marginRight:"20px"}}>
+                    <a  href="https://www.github.com/whiteb38" target="_blank" style={{backgroundImage:"url(../extra/Webp.net-resizeimage.png)", width:"90px", height:"20px", backgroundRepeat: "no-repeat", marginTop:"6px", marginRight:"20px"}}>
                     </a>
                   </li>
                   <li>
-                    <a className="navbar-text navbar-right" href="https://www.linkedin.com/in/brendon-white-55896b58/" target="_blank" style={{backgroundImage:"url(../extra/Logo-2C-21px-TM.png)", width:"100px", height:"20px", backgroundRepeat: "no-repeat", marginTop:"14px"}}>
+                    <a  href="https://www.linkedin.com/in/brendon-white-55896b58/" target="_blank" style={{backgroundImage:"url(../extra/Logo-2C-21px-TM.png)", width:"100px", height:"20px", backgroundRepeat: "no-repeat", marginTop:"14px"}}>
                     </a>
                   </li>
                 </ul>
               </div>
             </div>
           </nav>
+        </div>
     );
   }
 }
